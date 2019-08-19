a563f80c55e562fa48ade0fcc2029be5d4bd7ada
Sergey Ermakov <ermakov42@mail.ru>
2019-08-19 19:43:52 +0700

    Final edition and change version to 8

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,6 +1,11 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
 
 public class Starter
 {
@@ -8,14 +13,32 @@ public class Starter
     {
         try
         {
-            String url = "https://lenta.ru";
-            Document document = Jsoup.connect(url).get();
+            Document document = Jsoup.connect("https://lenta.ru").get();
             Elements images = document.select("img[src~=(.+)(png|jpg)]");
-            images.forEach(image -> System.out.println(image.attr("src")));
+            images.forEach(Starter::parceImageElement);
+        }
+        catch (Exception ex) {
+            System.out.println(ex.getMessage()); }
+        finally {
+            System.out.println("\nComplete !!!\n");
+        }
+    }
+
+    private static void parceImageElement(Element image)
+    {
+        try
+        {
+            String[] tokens = image.attr("src").split("/");
+            String fileName = tokens[tokens.length - 1];
+            URL url = new URL(image.attr("src"));
+            BufferedImage input = ImageIO.read(url);
+            String pathname = "images/" + fileName;
+            File output = new File(pathname);
+            ImageIO.write(input, "PNG", output);
         }
         catch (Exception ex)
         {
-            System.out.println(ex.getMessage());
+            ex.printStackTrace();
         }
     }
 }
